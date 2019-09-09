a6f7dc858e9ea0e55ffb55a5738723a215b86370
c212 <40395648+c212@users.noreply.github.com>
2019-09-09 15:14:29 -0400

    user input
    one time

--- 0909/One.java
+++ 0909/One.java
@@ -1,5 +1,13 @@
+import java.util.Scanner; 
+
 public class One {
   public static void main(String[] args) {
-     
+    Scanner comet;
+    String line; 
+    comet = new Scanner(System.in); 
+    System.out.println("Enter the first time: "); 
+    line = comet.nextLine();
+    System.out.println("You typed: " + line); 
+    
   }
 }
