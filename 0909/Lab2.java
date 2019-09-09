c735f71bb40ba2276b5351d9f39b22dc3acb06cc
c212 <40395648+c212@users.noreply.github.com>
2019-09-09 15:43:46 -0400

    fixed three issues
    check template

--- 0909/Lab2.java
+++ 0909/Lab2.java
@@ -1,18 +1,21 @@
 //import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JComponent;
 import java.awt.Graphics;
 import java.awt.Color;
 //import java.awt.Polygon;
 
-public class Lab2 {
+public class Lab2  extends JComponent {
   public static void main(String[] args) {
     JFrame a = new JFrame();
     int width = 500, height = 500;
     a.setVisible(true);
     a.setSize(width+25, height+75);
     a.setDefaultCloseOperation(3);
+    Lab2 b = new Lab2();
+    a.add(b);
   }
-  public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
     g.drawOval(0, 0, 500, 500);
     g.setColor(Color.WHITE);
     g.fillRect(0, 0, 500, 500);
@@ -20,4 +23,5 @@ public class Lab2 {
     g.fillRect(50, 50, 100, 100);
     
   }
+
 }
