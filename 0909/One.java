9bf2b7d6ab409bfe69461978516940c9beb45c68
c212 <40395648+c212@users.noreply.github.com>
2019-09-09 15:26:14 -0400

    started conversion
    still acting as a driver

--- 0909/One.java
+++ 0909/One.java
@@ -7,7 +7,9 @@ public class One {
     comet = new Scanner(System.in); 
     System.out.println("Enter the first time: "); 
     line = comet.nextLine();
-    System.out.println("You typed: " + line); 
-    
+    // System.out.println("You typed: " + line); 
+    int h = Integer.parseInt( line.substring(0, 2) ); 
+    int m = Integer.parseInt( line.substring(3) );
+    System.out.println( line + " is " + (h * 60 + m) + " in minutes " );
   }
 }
