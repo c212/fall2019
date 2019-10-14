776356e4e1cb119fab9a392271880b02504ad1e0
c212 <40395648+c212@users.noreply.github.com>
2019-10-14 15:07:40 -0400

    getting started

---
+++ 1014/Sequence.java
@@ -0,0 +1,10 @@
+// sequence: [1, 3, 2, 4, 5, 1, 9, 7]
+//            ----
+// prefix: I see 8 (possibly nine)
+// longest sorted (ascending) prefix: [1, 3]
+// [1, 3, 5, 7, 2, 4, 6, 8] 
+// ------------
+// let's implement Sequence
+public class Sequence {
+  
+}
