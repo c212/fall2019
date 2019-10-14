8a40071aec96482ee78cb2c5c6e25eebdf4cf911
c212 <40395648+c212@users.noreply.github.com>
2019-10-14 15:19:41 -0400

    sequence objects can now store numbers
    they can also tell you how many

--- 1014/Sequence.java
+++ 1014/Sequence.java
@@ -5,6 +5,14 @@
 // [1, 3, 5, 7, 2, 4, 6, 8] 
 // ------------
 // let's implement Sequence
+import java.util.ArrayList; 
+
 public class Sequence {
-  
+  private ArrayList<Integer> values = new ArrayList<Integer>(); 
+  public Sequence() {
+    
+  }
+  public int size() {
+    return this.values.size();  
+  }
 }
